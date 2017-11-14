f627648d3054172fc2a90ddd51b2f2db057b1666
Plínio Naves <pliniopjn@gmail.com>
2017-11-14 19:14:55 +0000

    update to last version of ng2-pdf-viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { PdfViewerComponent } from 'ng2-pdf-viewer';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 import { HomePage } from '../pages/home/home';
 import { MyApp } from './app.component';
@@ -14,12 +14,12 @@ import { PdfViewerPage } from './../pages/pdf-viewer/pdf-viewer';
   declarations: [
     MyApp,
     HomePage,
-    PdfViewerComponent,
     PdfViewerPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    PdfViewerModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
